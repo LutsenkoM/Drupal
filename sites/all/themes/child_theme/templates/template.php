ec617e724fb79310388c5ae3976c4023434de576
Maksym Lutsenko <maksym.m.lutsenko@gmail.com>
2019-01-31 19:26:42 +0200

    by documentation

---
+++ sites/all/themes/child_theme/templates/template.php
@@ -0,0 +1,10 @@
+<?php
+
+function child_theme_process_page(&$vars) {
+
+  
+
+}
+
+function child_theme_preprocess_html(&$variables) {
+  drupal_add_css('http://fonts.googleapis.com/css?family=News+Cycle', array('type' => 'external')); }
