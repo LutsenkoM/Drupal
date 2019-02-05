a0c6267d97877636f83d83f4e6dfd0730dd50986
Maksym Lutsenko <maksym.m.lutsenko@gmail.com>
2019-02-05 15:31:00 +0200

    Module create

--- sites/all/themes/child_theme/template.php
+++ sites/all/themes/child_theme/template.php
@@ -5,8 +5,8 @@ function child_theme_preprocess_page(&$variables) {
   $variables['p_page'] = 'Variable from preprocess page';
 
   if (isset($variables['node']->type)) {
-//    $nodetype = $variables['node']->type;
-//    $variables['theme_hook_suggestions'][] = 'page__' . $nodetype;
+    $nodetype = $variables['node']->type;
+    $variables['theme_hook_suggestions'][] = 'page__' . $nodetype;
 
     // If the node type is "blog" the template suggestion will be "page--blog.tpl.php".
     $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
