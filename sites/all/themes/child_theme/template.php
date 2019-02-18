2adc27e3a23010abbbae88f9d48e561bc932a23c
Max Lutsenko <lutsenko.maxim@gmail.com>
2019-02-18 23:09:00 +0200

    Blocks with teams and members

--- sites/all/themes/child_theme/template.php
+++ sites/all/themes/child_theme/template.php
@@ -22,65 +22,65 @@ function child_theme_preprocess_html(&$vars) {
   $vars['p_html'] = 'Variable from preprocess_html';
 }
 
-function child_theme_preprocess_node(&$vars) {
-  if (drupal_is_front_page()) {
-    $teams_home_id = taxonomy_vocabulary_machine_name_load('team');
-    $teams_home = entity_load('taxonomy_term', FALSE, array('vid' => $teams_home_id->vid));
+//function child_theme_preprocess_node(&$vars) {
+//  if (drupal_is_front_page()) {
+//    $teams_home_id = taxonomy_vocabulary_machine_name_load('team');
+//    $teams_home = entity_load('taxonomy_term', FALSE, array('vid' => $teams_home_id->vid));
+//
+//    $location_home_id = taxonomy_vocabulary_machine_name_load('location');
+//    $locations_home = entity_load('taxonomy_term', FALSE, array('vid' => $location_home_id->vid));
+//
+//    $locs_home = array();
+//
+//    foreach ($locations_home as $loc_home) {
+//
+//      $sorted_teams = array();
+//
+////    Hook_menu, hook_theme. Custom link with homepage blocks
+//      foreach ($teams_home as $team_home) {
+//        if ($team_home->field_team_location['und'][0]['tid'] == $loc_home->tid) {
+//          $sorted_teams[] = $team_home;
+//        }
+//      }
+//
+//      $loc_home->teams = $sorted_teams;
+//      $locs_home[] = $loc_home;
+//    }
+//
+//    $vars['locations_home'] = $locs_home;
+//  }
+//}
 
-    $location_home_id = taxonomy_vocabulary_machine_name_load('location');
-    $locations_home = entity_load('taxonomy_term', FALSE, array('vid' => $location_home_id->vid));
-
-    $locs_home = array();
-
-    foreach ($locations_home as $loc_home) {
-
-      $sorted_teams = array();
-
-//    Hook_menu, hook_theme. Custom link with homepage blocks
-      foreach ($teams_home as $team_home) {
-        if ($team_home->field_team_location['und'][0]['tid'] == $loc_home->tid) {
-          $sorted_teams[] = $team_home;
-        }
-      }
-
-      $loc_home->teams = $sorted_teams;
-      $locs_home[] = $loc_home;
-    }
-
-    $vars['locations_home'] = $locs_home;
-  }
-}
-
-function child_theme_preprocess_taxonomy_term(&$vars) {
-  $vars['p_taxonomy_term'] = 'Variable from preprocess_taxonomy term';
-
-  $tax_id = $vars['tid'];
-
-//  Get members
-  $nodes_id = taxonomy_select_nodes($tax_id);
-  $nodes = array();
-
-  foreach ($nodes_id as $nod_id) {
-    $node = node_load($nod_id);
-    array_push($nodes, $node);
-  }
-
-  $vars['members'] = $nodes;
-
-//    Get teams
-  $vocabulary = taxonomy_vocabulary_machine_name_load('team');
-  $teams_term = entity_load('taxonomy_term', FALSE, array('vid' => $vocabulary->vid));
-  $output_teams = array();
-
-  foreach ($teams_term as $team) {
-    if ($team->field_team_location['und'][0]['tid'] == $tax_id) {
-      $output_teams[] = $team;
-    }
-  }
-
-  $vars['teams'] = $output_teams;
-
-}
+//function child_theme_preprocess_taxonomy_term(&$vars) {
+//  $vars['p_taxonomy_term'] = 'Variable from preprocess_taxonomy term';
+//
+//  $tax_id = $vars['tid'];
+//
+////  Get members
+//  $nodes_id = taxonomy_select_nodes($tax_id);
+//  $nodes = array();
+//
+//  foreach ($nodes_id as $nod_id) {
+//    $node = node_load($nod_id);
+//    array_push($nodes, $node);
+//  }
+//
+//  $vars['members'] = $nodes;
+//
+////    Get teams
+//  $vocabulary = taxonomy_vocabulary_machine_name_load('team');
+//  $teams_term = entity_load('taxonomy_term', FALSE, array('vid' => $vocabulary->vid));
+//  $output_teams = array();
+//
+//  foreach ($teams_term as $team) {
+//    if ($team->field_team_location['und'][0]['tid'] == $tax_id) {
+//      $output_teams[] = $team;
+//    }
+//  }
+//
+//  $vars['teams'] = $output_teams;
+//
+//}
 
 function child_theme_preprocess_block(&$variables) {
   $variables['theme_hook_suggestions'][] = 'block__' . $variables['block']->region;
--- sites/all/themes/child_theme/templates/taxonomy-term.tpl.php
+++ sites/all/themes/child_theme/templates/taxonomy-term.tpl.php
@@ -1,26 +1,7 @@
 <?php
 ?>
 <div id="taxonomy-term-<?php print $term->tid; ?>" class="<?php print $classes; ?>">
-    <div class="col-md-9">
+    <div class="col-md-12">
         <?php print render($content['description'])?>
-    </div>
-    <div class="col-md-3">
-      <?php if(!empty($teams)) { ?>
-          <h2>Teams</h2>
-        <?php
-        foreach ($teams as $team) {
-          $tid = $team->tid;
-          $url = entity_uri('taxonomy_term', $team);
-          print '<div><a href="/' . $url['path'] . '">' . $team->name . '</a></div>';
-        }
-      } elseif (!empty($members)) { ?>
-          <h2>Members</h2>
-        <?php
-        foreach ($members as $member) {
-          $display = array('label' => 'hidden');
-          $member_link = field_view_field('node', $member, 'field_member_link', $display);
-          print render($member_link);
-        }
-      } ?>
     </div>
 </div>
